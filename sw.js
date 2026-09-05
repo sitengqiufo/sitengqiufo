5b4bc4e66f314cc2ac36971986905daa6e00da07
sitengqiufo <15291756897@139.com>
2026-09-05 16:57:25 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,10 +1,14 @@
-const CACHE='yundian-v1';
+const CACHE='yundian-v2';
 const ASSETS=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
 self.addEventListener('fetch',e=>{
   if(e.request.method!=='GET')return;
+  if(e.request.mode==='navigate'||e.request.url.endsWith('index.html')){
+    e.respondWith(fetch(e.request).then(res=>{const c=res.clone();caches.open(CACHE).then(cc=>cc.put(e.request,c));return res}).catch(()=>caches.match('./index.html')));
+    return;
+  }
   e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(res=>{
     const copy=res.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return res;
-  }).catch(()=>caches.match('./index.html'))));
+  })));
 });
